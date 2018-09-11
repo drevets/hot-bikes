933214221db2478bb471a64d410a7c8629632634
Drevets <drevets@gmail.com>
2018-09-11 11:59:45 -0500

    put markers on map

--- chicago_map.py
+++ chicago_map.py
@@ -1,22 +1,39 @@
 import pandas as pd
 import folium
+import requests
 
-#why these modules no exist?
+#should probably put all of this into a method or something..?
 
 bike_data = pd.read_csv("Divvy_Trips_2018_06.csv")
 
 response = requests.get('https://feeds.divvybikes.com/stations/stations.json')
 
-# man there is so much that I'm not understanding about this....
-
 station_data = response.json()
+station_list = station_data['stationBeanList']
 
-def put_stations_on_map(station_data):
-    folium_map = folium.Map(location=[41.881, ‎-87.623],
-                zoom_start = 13,
-                 tiles = "CartoDB dark_matter")
-    for station in station_data:
-        folium.CircleMarker(location=[station.latitude, station.longitude], fill=True).add_to(folium_map)
+def put_stations_on_map(station_list):
+    folium_map = folium.Map(location=[41.88, -87.62],
+                            zoom_start=13,
+                            tiles="CartoDB dark_matter")
+    for station in station_list:
+        folium.CircleMarker(location=[station['latitude'], station['longitude']], fill=True).add_to(folium_map)
+    folium_map.save("marker_map.html")
     return folium_map
 
-#so I think this will return a map with stations on it....
+
+marker_map = put_stations_on_map(station_list)
+
+#how do I display the map without a web browser? where do I host this in general?
+
+# #so I think this will return a map with stations on it....
+#
+# #so now I want to take that map and count the number of trips from each station. This will be harder.
+# #or, I think what I really want to do is add the station latitude and longitude to the map, and then
+# #add station latitude and longitude to the map that I have, based on station id, so that I can then group by total number of
+# #depatures.
+#
+# def get_trip_counts_by_station(station_id):
+#     departure_counts = subset.groupby('from_station_id').count()
+#     departure_counts.columns = ['Departure Count']
+
+
