7a6955626c910146a1e04da5601c7762e56e9c90
Drevets <drevets@gmail.com>
2018-11-08 09:50:01 -0600

    Updated webpack to include html-loader

---
+++ client/src/components/Map.js
@@ -0,0 +1,15 @@
+//import React, { Component } from 'react';
+//import __map_html from '../../../script/one_trip.html';
+//const map_template = {__map_html: __html };
+//
+//class Map extends Component {
+//  render() {
+//    return (
+//        <div dangerouslySetInnerHTML={map_template} />
+//    );
+//  }
+//}
+//
+//export default Map;
+//
+////to do —how can I display my html map here
