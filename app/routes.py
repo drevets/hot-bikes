88b7a3863279e6f91d60f3a7afe4c499ed6083d2
Drevets <drevets@gmail.com>
2018-11-12 20:58:52 -0600

    Added redirect so people can start over after finding a match

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 from source.constants import station_list
 from source.utility import get_hour_from_time_string
 from source.make_route_map import find_intersecting_routes_and_save_map_html
@@ -22,4 +22,11 @@ def homepage():
         print('This is your end station', end_station)
         print('this is your time', hour)
         find_intersecting_routes_and_save_map_html(gender, hour, start_station, end_station)
+        return redirect(location='/intersect')
+
+@bp.route('/intersect', methods=('GET', 'POST'))
+def intersect():
+    if request.method == 'GET':
         return render_template('intersecting_routes.html')
+    elif request.method == 'POST':
+        return redirect('/')
